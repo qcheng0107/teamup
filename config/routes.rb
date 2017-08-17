5ccacb7dab34a09c2248c4f3b4efdde8b289b4a5
Raghu Betina <raghu@firstdraft.com>
2017-08-17 01:36:52 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "sessions#index"
   # Routes for the Tagging resource:
   # CREATE
